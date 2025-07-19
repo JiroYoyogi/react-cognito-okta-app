5efae68d089a86f251e0e650e644bde64072699b
JiroYoyogi <jiro.yoyogi.v5@gmail.com>
2025-07-19 18:27:22 +0900

    変数設定ファイル整理

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -1,17 +1,18 @@
-// https://ap-northeast-1abcd.auth.ap-northeast-1.amazoncognito.com
+// e.g. https://ap-northeast-1ga5zacsn4.auth.ap-northeast-1.amazoncognito.com
 export const cognitoDomain= "";
-// https://trial-12345.okta.com
-export const oktaDomain = "";
-export const appDomain = "http://localhost:5173";
-
-export const cognitoLogoutUri = `${appDomain}/callback-logout/`;
-export const cognitoClientId = "";
+// e.g. ap-northeast-1_Ga5ZacSN4
 const cognitoUserPoolId = "";
+// e.g. 2hoqf7stavskccjltmtpm93hsq
+export const cognitoClientId = "";
+// e.g. "https://trial-12345.okta.com"
+export const oktaDomain = "";
+
+export const appDomain = "http://localhost:5173";
 
 export const cognitoConfig = {
   authority: `https://cognito-idp.ap-northeast-1.amazonaws.com/${cognitoUserPoolId}`,
   client_id: cognitoClientId,
-  redirect_uri: `${appDomain}/callback-login/`,
+  redirect_uri: `${appDomain}/`,
   response_type: "code",
   scope: "email openid phone",
 };

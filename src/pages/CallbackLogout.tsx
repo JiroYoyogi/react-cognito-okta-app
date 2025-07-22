e86b0fcde24a8c55c806c822ca6f9ecf7353d3f6
JiroYoyogi <jiro.yoyogi.v5@gmail.com>
2025-07-22 22:54:50 +0900

    コメント追加

--- src/pages/CallbackLogout.tsx
+++ src/pages/CallbackLogout.tsx
@@ -5,6 +5,7 @@ import { oktaDomain, appDomain } from "../authConfig";
 const CallbackLogout = () => {
   useEffect(() => {
     // OktaのログアウトURLへリダイレクト
+    // fromURIはOktaでログアウトした後のリダイレクト先
     const oktaLogoutUrl = `${oktaDomain}/login/signout?fromURI=${encodeURIComponent(appDomain)}`;
     window.location.href = oktaLogoutUrl;
   }, []);
